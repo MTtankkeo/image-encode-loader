9d48569e2ec1c08095438afc471039b4da10badf
Dev Ttangkong <ttankkeo112@gmail.com>
2024-09-06 06:50:22 +0900

    Update /dist files

--- dist/types/types.d.ts
+++ dist/types/types.d.ts
@@ -1,8 +1,5 @@
-
 /** Signature for the output image path type for `image-encode-loader`. */
 type ImageEncodeEmitPath = string;
-
-// Signature for the image path of file `PNG` extension and format type.
 declare module '*.{png,jpg,jpeg,webp,avif,heic,heif,gif,tiff' {
     const value: ImageEncodeEmitPath;
     export default value;
